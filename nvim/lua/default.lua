126f88f59fabba88e53220ce3660e984819bbde4
joey0320 <joonho0320@gmail.com>
2024-04-03 00:09:10 -0700

    mac space problem

--- nvim/lua/default.lua
+++ nvim/lua/default.lua
@@ -65,7 +65,8 @@ cmp.setup {
 vim.api.nvim_create_user_command('Rfinder',
     function()
         local path = vim.api.nvim_buf_get_name(0)
-        os.execute('open ' .. path)
+        local no_space_path = string.gsub(path, "%s+", "\\ ")
+        os.execute('open ' .. no_space_path)
     end,
     {}
 )
